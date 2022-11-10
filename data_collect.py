7a48a006a760a966dac88b839915aeac61874979
Suhwan <ksk031300@naver.com>
2022-11-10 15:41:35 +0900

    scroll_height

--- data_collect.py
+++ data_collect.py
@@ -4,6 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
+def selenium_scroll_option():
+    SCROLL_PAUSE_SEC = 3
+
+    # 스크롤 높이 가져옴
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
 
 chrome_options = webdriver.ChromeOptions()
 base_url = "https://www.google.co.kr/imghp?hl=ko"
@@ -14,3 +20,5 @@ driver.get(base_url)
 browser = driver.find_element(By.NAME, "q")
 browser.send_keys(word)
 browser.send_keys(Keys.RETURN)
+
+seleniu,_scroll_option()
