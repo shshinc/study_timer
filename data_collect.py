e432212d9fae1a1d0ec356c7579d2ebbb27bda7d
Suhwan <ksk031300@naver.com>
2022-11-10 15:34:01 +0900

    setting

---
+++ data_collect.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
