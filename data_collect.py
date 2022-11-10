971976faaa8d908d6c4c09abd6cc90768018f1da
Suhwan <ksk031300@naver.com>
2022-11-10 15:36:23 +0900

    searching

--- data_collect.py
+++ data_collect.py
@@ -7,4 +7,10 @@ import urllib.request
 
 chrome_options = webdriver.ChromeOptions()
 base_url = "https://www.google.co.kr/imghp?hl=ko"
-words = "pen"
+word = "pen"
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(base_url)
+browser = driver.find_element(By.NAME, "q")
+browser.send_keys(word)
+browser.send_keys(Keys.RETURN)
