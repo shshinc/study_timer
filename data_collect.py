606605f822c16cf6163447716bd8b742d2f77190
Suhwan <ksk031300@naver.com>
2022-11-10 15:35:49 +0900

    data_collect_pen

--- data_collect.py
+++ data_collect.py
@@ -4,3 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
+
+chrome_options = webdriver.ChromeOptions()
+base_url = "https://www.google.co.kr/imghp?hl=ko"
+words = "pen"
