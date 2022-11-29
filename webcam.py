bda3491328591bd11cd79de63cd46af478f9b6bd
shshinc <ksk031300@naver.com>
2022-11-29 23:29:16 +0900

    YOLO network 구성

--- webcam.py
+++ webcam.py
@@ -4,7 +4,11 @@ VideoSignal = cv2.VideoCapture(0)
 # YOLO 가중치 파일과 CFG 파일 로드
 YOLO_net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 
-
+# YOLO NETWORK 재구성
+classes = []
+with open("yolo.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+    
 if not webcam.isOpened():
     print("Could not open webcam")
     exit()
