700e81dd78181ceaeb841edb477f762f811eb0b0
shshinc <ksk031300@naver.com>
2022-11-29 23:28:39 +0900

    YOLO3사용

--- webcam.py
+++ webcam.py
@@ -1,6 +1,9 @@
 import cv2
+import numpy as np
+VideoSignal = cv2.VideoCapture(0)
+# YOLO 가중치 파일과 CFG 파일 로드
+YOLO_net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 
-webcam = cv2.VideoCapture(0)
 
 if not webcam.isOpened():
     print("Could not open webcam")
