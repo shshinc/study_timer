6d0686633fe28faeeb5ba36afbc190a9deddf8dd
9incero <9incero@gmail.com>
2022-12-02 21:41:47 +0900

    아이디 미입력시 팝업설정

---
+++ 새 폴더/login.js
@@ -0,0 +1,22 @@
+const id = document.getElementById('id')
+const password = document.getElementById('password')
+const login = document.getElementById('login')
+
+login.addEventListener('click', () => {
+    if (id.value == 'id') {
+        if (password.value == '0000') {
+            alert('로그인에 성공하였습니다.')
+            location.href="index.html";
+        }
+        else {
+            alert('아이디와 비밀번호를 다시 확인해주세요.')
+        }
+    }
+    if (id.value==''){
+        alert('아이디와 비밀번호를 입력해주세요.')
+    }
+    else {
+        alert('존재하지 않는 계정입니다.')
+    }
+
+})
