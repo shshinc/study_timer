2cc3a55f6c774e12b454ccac57578dc662b90af9
shshinc <ksk031300@naver.com>
2022-12-02 21:29:09 +0900

    error 해결 및 encode image

--- roboflow.py
+++ roboflow.py
@@ -29,9 +29,12 @@ video = cv2.VideoCapture(0)
 def infer():
     ret, img = video.read()
 
-    height, width, channels = img.shape()
+    height, width, channels = img.shapee
     scale = ROBOFLOW_SIZE / max(height, width)
     img = cv2.resize(img, (round(scale * width), round(scale * height)))
+    
+    retval, buffer = cv2.imencode('.jpg', img)
+    img_str = base64.b64encode(buffer)
 
 while 1:
     if(cv2.waitKey(1) == ord('q')):
