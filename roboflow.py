83babe676dbc97ce617b96376e1d72b5a83fb4f3
shshinc <ksk031300@naver.com>
2022-12-02 21:23:36 +0900

    api key, model size setting

--- roboflow.py
+++ roboflow.py
@@ -6,3 +6,7 @@ import time
 import json
 with open('model.json') as f:
     config = json.load(f)
+
+    ROBOFLOW_API_KEY = json.config["ROBOFLOW_API_KEY"]
+    ROBOFLOW_MODEL = json.config["ROBOFLOW_MODEL"]
+    ROBOFLOW_SIZE = json.config["ROBOFLOW_SIZE"]
