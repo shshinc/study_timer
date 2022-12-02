86935c2a840ab3b07a1ff938017ea5bb302afd5b
shshinc <ksk031300@naver.com>
2022-12-02 21:23:49 +0900

    error 해결

--- roboflow.py
+++ roboflow.py
@@ -7,6 +7,6 @@ import json
 with open('model.json') as f:
     config = json.load(f)
 
-    ROBOFLOW_API_KEY = json.config["ROBOFLOW_API_KEY"]
-    ROBOFLOW_MODEL = json.config["ROBOFLOW_MODEL"]
-    ROBOFLOW_SIZE = json.config["ROBOFLOW_SIZE"]
+    ROBOFLOW_API_KEY = config["ROBOFLOW_API_KEY"]
+    ROBOFLOW_MODEL = config["ROBOFLOW_MODEL"]
+    ROBOFLOW_SIZE = config["ROBOFLOW_SIZE"]
