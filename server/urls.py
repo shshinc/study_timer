400decae135238f8fd43ea53b4a89c34d506a882
miseeo <oasiwm@gmail.com>
2022-12-08 01:51:54 +0900

    비밀번호 보기/숨기기

--- server/urls.py
+++ server/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
-    # path('accounts/', include('accounts.urls')),
     path('account/',include('account.urls')),
 ]
