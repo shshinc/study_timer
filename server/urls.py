76d329f3904935a1468dc82ecec4d1a5318f963b
9incero <9incero@gmail.com>
2022-12-07 10:49:20 +0900

    경로 설정 및 시간대 설정

--- server/urls.py
+++ server/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import server.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', server.views.index, name='index'),
 ]
