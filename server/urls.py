448c1efd4765a34458957bbc0b1bd16c212336e7
9incero <9incero@gmail.com>
2022-12-07 22:15:11 +0900

    post 값보내기

--- server/urls.py
+++ server/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', server.views.index, name='index'),
     # path('accounts/', include('accounts.urls')),
+    path('', server.views.post, name='post'),
 ]
--- server/views.py
+++ server/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 def index(request):
     return render(request, 'index.html')
+
+def post(request):
+    #post방식
+    if request.method == 'POST':
+        post = Post()
+        post.text = request.POST['text']
+        post.save()
+        return redirect('post')
+    #get방식
+    else:
+        post = Post.objects.all()
+        return render(request, 'index.html', {'post':post})
