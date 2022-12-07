663e4da70a1eba7023cc86b4b0f93c40ecd2a496
9incero <9incero@gmail.com>
2022-12-07 11:21:07 +0900

    가상환경 설치 및 오류수정

--- server/settings.py
+++ server/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
