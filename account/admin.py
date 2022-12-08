9ec3dab6f2224cd2d3b162cc65b32ecddeba1596
miseeo <oasiwm@gmail.com>
2022-12-08 22:41:09 +0900

    admin.py에 models.py 연결

--- account/admin.py
+++ account/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import User   #같은 경로의 models.py에서 User라는 클래스를 임포트한다.
+
+# Register your models here.
+
+class UserAdmin(admin.ModelAdmin) :
+    list_display = ('username', 'password')
+
+
+admin.site.register(User, UserAdmin) #site에 등록
