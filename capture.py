0e51f1e4dac97a73ac25faf51e2a1ddf181f6806
shshinc <ksk031300@naver.com>
2022-12-09 23:58:37 +0900

    물체의 이름 정보 받기

--- capture.py
+++ capture.py
@@ -4,4 +4,7 @@ import datetime
 
 def get_study_time():
     net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("yolo.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
     return
