a6e7a4188cf7a4a5825835de70b71c264ccad7f9
shshinc <ksk031300@naver.com>
2022-12-09 23:59:26 +0900

    신경망 생성

--- capture.py
+++ capture.py
@@ -7,4 +7,7 @@ def get_study_time():
     classes = []
     with open("yolo.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
     return
