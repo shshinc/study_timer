756c5a5718dab8ed8dfc77803142704d7f815968
9incero <9incero@gmail.com>
2022-12-10 06:02:53 +0900

    그래프 디자인 변경 및 쓸모없는 코드 삭제

--- main/static/main.js
+++ main/static/main.js
@@ -19,14 +19,6 @@ function startClock() {
 }
 
 
-// 시간(int)을 시, 분, 초 문자열로 변환
-function getTimeFormatString() {
-    hour = parseInt(String(time / (60 * 60)));
-    min = parseInt(String((time - (hour * 60 * 60)) / 60));
-    sec = time % 60;
-
-    return String(hour).padStart(2, '0') + ":" + String(min).padStart(2, '0') + ":" + String(sec).padStart(2, '0');
-}
 
 
 
