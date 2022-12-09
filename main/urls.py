38827f5601daa9f04f2cb9900b8340d825543b43
9incero <9incero@gmail.com>
2022-12-10 06:05:59 +0900

    데이터 전송 경로 변경

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ import main.views
 urlpatterns = [
     path('', index, name='index'),
     path('after/', main.views.after),
-    path('after/end/', main.views.image),
+    path('end/', main.views.image),
+    path('after/end', main.views.after),
 ]
 
