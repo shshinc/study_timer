d2f48961e88b3782fd8b56ebff21a148b35aaaad
miseeo <oasiwm@gmail.com>
2022-12-09 23:12:37 +0900

    회원가입 db연결 오류메시지 수정

--- account/views.py
+++ account/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.contrib import auth
 from django.contrib.auth import authenticate
@@ -19,17 +17,6 @@ def signup(request):
         email = request.POST.get('email')
         
         response = {}
-        # if password != re_password:
-        #     response['error'] = '비밀번호를 확인해주세요.'
-        # if not username:
-        #     response['error'] = '이름을 입력해주세요.'
-        # if not password:
-        #     response['error'] = '비밀번호를 입력해주세요.'
-        # if not re_password:
-        #     response['error'] = '비밀번호를 다시 입력해주세요.'
-        # if (not username) and (not password) and (not re_password):
-        #     response['error'] = '모든 값을 입력해주세요.'
-        # if not response['error']:
         users = User(
             username = username,
             password = password,
@@ -37,4 +24,3 @@ def signup(request):
         )
         users.save()
         return render(request, 'signup.html')
-        # return render(request, 'signup.html', {'response': response['error']})
