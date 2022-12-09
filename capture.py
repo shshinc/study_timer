a8887fb5c90bac04316ca148ea58658adcfec5b4
shshinc <ksk031300@naver.com>
2022-12-10 00:00:09 +0900

    저장된 이미지 파일 읽기

--- capture.py
+++ capture.py
@@ -10,4 +10,8 @@ def get_study_time():
     layer_names = net.getLayerNames()
     output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    count = 1
+    img = cv2.imread("C:/Users/ksk03/Downloads/image(" + str(count) + ").jpg")
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
     return
